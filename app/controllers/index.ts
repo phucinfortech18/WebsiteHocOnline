189e87e89c30fe32ed90b700f99f759cbba3ca6b
Anh Phuc <phucinfortech18@gmail.com>
2018-10-06 18:08:51 +0700

    update 2

---
+++ app/controllers/index.ts
@@ -0,0 +1,13 @@
+import * as $ from 'jquery';
+import 'bootstrap';
+import 'popper.js';
+import 'jquery';
+// import 'font-awesome';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+import '../assets/sass/main.scss';
+
+$('#btndemo').click(function(){
+    alert(2);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const path = require('path');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
+    mode: 'development',
     entry: './app/controllers/index.ts',
     devtool: 'source-map',
     module: {
         rules: [
-            //Typescript loader
+            //Dong goi Typescript loader
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
             },
+            //Dong goi FontAwesome
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',    // where the fonts will go
+                       // publicPath: '../'       // override the default path
+                    }
+                }]
+            },
+
             //SASS LOADER ĐỂ ĐÓNG GÓI CÁC FILE SASS
             {
                 test: /\.scss$/,
@@ -55,4 +70,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './app')
     },
+    // plugins: [
+    //     new ExtractTextPlugin("styles.css"),
+    //   ]
 };
